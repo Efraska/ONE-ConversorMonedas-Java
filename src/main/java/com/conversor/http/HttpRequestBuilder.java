22c018e763994ccad707abe3209146f1b362e098
Efraska <aux_sistemas@medicainsurgentes.com.mx>
2024-12-09 14:55:59 -0600

    refactor: ajustes en la construcción de solicitudes HTTP

--- src/main/java/com/conversor/http/HttpRequestBuilder.java
+++ src/main/java/com/conversor/http/HttpRequestBuilder.java
@@ -1,2 +1,19 @@
-package com.conversor.http;public class HttpRequestBuilder {
+package com.conversor.http;
+
+import java.net.URI;
+import java.net.http.HttpRequest;
+
+public class HttpRequestBuilder {
+    private static final String API_URL = "https://v6.exchangerate-api.com/v6/";
+
+    public static HttpRequest buildGetRequest(String apiKey, String baseCurrency) {
+        String endpoint = API_URL + apiKey + "/latest/" + baseCurrency;
+
+        // Crear una solicitud GET personalizada
+        return HttpRequest.newBuilder()
+                .uri(URI.create(endpoint)) // Define el URI del endpoint
+                .header("Accept", "application/json") //Configura el encabezado
+                .GET() //Define el método HTTP como GET
+                .build();
+    }
 }
